b7859a82ac89d08d1e19cfa495dc58d6f9238237
Matej Ramuta <matej.ramuta@gmail.com>
2019-05-30 18:37:04 -0400

    solution to the homework 1 in lesson 6

--- lesson-06/basketball-football-players/example_b.py
+++ lesson-06/basketball-football-players/example_b.py
@@ -72,3 +72,23 @@ print(messi.goals)
 print(messi.weight_to_lbs())
 
 print(lebron.__dict__)
+
+# user can enter data
+print()  # empty line
+print("Enter some football player's data!")
+f_name = input("Enter player's first name: ")
+l_name = input("Enter player's last name: ")
+height = input("Enter player's height: ")
+weight = input("Enter player's weight: ")
+goals = input("Enter the number of player's goals: ")
+y_cards = input("Enter the number of player's yellow cards: ")
+r_cards = input("Enter the number of player's red cards: ")
+
+new_player = FootballPlayer(first_name=f_name, last_name=l_name, height_cm=float(height), weight_kg=float(weight),
+                            goals=int(goals), yellow_cards=int(y_cards), red_cards=int(r_cards))
+
+with open("football_players.txt", "w") as football_file:
+    football_file.write(str(new_player.__dict__))
+
+print("Player successfully entered!")
+print("Player's data: {0}".format(new_player.__dict__))

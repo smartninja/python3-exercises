a5d6730f1deb4dfd4c33f5b234dc685e60729e22
Matej Ramuta <matej.ramuta@gmail.com>
2020-08-18 12:39:00 +0200

    CSV file exercise solution

---
+++ lesson-03/csv-file/main.py
@@ -0,0 +1,6 @@
+with open("data.csv", 'r') as data_file:
+    content = data_file.readlines()
+
+    for row in content:
+        row_data = row.split(",")
+        print(f"{row_data[0]} is {row_data[1]} years old and {row_data[2]}")

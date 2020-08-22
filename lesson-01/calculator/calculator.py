682c848b695bb4a2eed349f0a832134dc4716ce5
Matej Ramuta <matej.ramuta@gmail.com>
2020-08-22 11:00:14 +0200

    missing parenthesis added

--- lesson-01/calculator/calculator.py
+++ lesson-01/calculator/calculator.py
@@ -2,7 +2,7 @@ x = int(input("Enter the value for x: "))
 
 y = int(input("Enter the value for y: "))
 
-operation = input("Choose math operation (+, -, *, /: ")
+operation = input("Choose math operation (+, -, *, /): ")
 
 if operation == "+":
     print(x + y)
